8cd49152c8201eb1ef66f0d0e99b347e9e5c0958
Raph <raphael@haddad.net.au>
2016-12-14 15:23:39 +1100

    make gulpfile.js compliant with lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-var gulp = require('gulp'),
-debug = require('gulp-debug'),
-lint = require('gulp-jslint'),
-src = ['main.js'];
+/*global require */
+(function () {
+    'use strict';
+    var gulp = require('gulp'),
+        debug = require('gulp-debug'),
+        lint = require('gulp-jslint'),
+        src = ['main.js', 'gulpfile.js'];
 
-gulp.task('default', function () {
-    gulp.src(src)
-    .pipe(debug({title: 'test'}))
-    .pipe(lint())
-    .pipe(lint.reporter('stylish'))
-});
+    gulp.task('default', function () {
+        gulp.src(src)
+            .pipe(debug({title: 'test'}))
+            .pipe(lint({browser: true}))
+            .pipe(lint.reporter('stylish'));
+    });
+}());
